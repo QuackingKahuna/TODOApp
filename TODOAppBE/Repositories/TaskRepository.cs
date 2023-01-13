ae25d9d7abd1be22d0974774d50027987638ba26
Pavel Němec <pav.nemec@email.cz>
2023-01-14 00:37:34 +0100

    Delete Task, Insert name condition

--- TODOAppBE/Repositories/TaskRepository.cs
+++ TODOAppBE/Repositories/TaskRepository.cs
@@ -1,16 +1,27 @@
 ﻿using TODOAppBE.Entities;
+using System.Linq;
 
 namespace TODOAppBE.Repositories
 {
     public interface ITaskRepository
     {
+        string Delete(string taskName);
         TaskEntity Get(string taskName);
         TaskEntity Insert(TaskEntity task);
     }
 
     public class TaskRepository : ITaskRepository
     {
-        private List<TaskEntity> TaskEntities = new List<TaskEntity>();
+        private IList<TaskEntity> TaskEntities = new List<TaskEntity>();
+
+        public string Delete(string taskName)
+        {
+            var entityToRemove = Get(taskName);
+            if (entityToRemove != null) 
+                TaskEntities.Remove(entityToRemove);
+
+            return entityToRemove?.Name;
+        }
 
         public TaskEntity Get(string taskName)
         {
@@ -19,6 +30,9 @@ namespace TODOAppBE.Repositories
 
         public TaskEntity Insert(TaskEntity task)
         {
+            if (TaskEntities.Any(x => x.Name == task.Name)) 
+                throw new Exception("This task already exists.");
+
             TaskEntities.Add(task);
             return task;
         }
--- TODOAppBeTests/TaskRepoTests.cs
+++ TODOAppBeTests/TaskRepoTests.cs
@@ -1,3 +1,5 @@
+using Shouldly;
+using System;
 using TODOAppBE.Entities;
 using TODOAppBE.Repositories;
 using Xunit;
@@ -11,6 +13,16 @@ namespace TODOAppBeTests
         public TaskRepoTests()
         {
             _sut = new TaskRepository();
+        }
+
+        [Fact]
+        public void InsertTask_ExistingName()
+        {
+            var input = new TaskEntity("Name", 1, TODOAppBE.Common.Status.NotStarted);
+            _sut.Insert(input);
+            var input2 = new TaskEntity("Name", 2, TODOAppBE.Common.Status.NotStarted);
+            
+            Should.Throw<Exception>(() => _sut.Insert(input2));
         }
 
         [Fact]
@@ -31,6 +43,28 @@ namespace TODOAppBeTests
             _sut.Insert(input);
 
             var res = _sut.Get("Name2");
+
+            Assert.Equivalent(null, res);
+        }
+
+        [Fact]
+        public void DeleteTask_ValidName()
+        {
+            var input = new TaskEntity("Name", 1, TODOAppBE.Common.Status.NotStarted);
+            _sut.Insert(input);
+
+            var res = _sut.Delete("Name");
+
+            Assert.Equivalent("Name", res);
+        }
+
+        [Fact]
+        public void DeleteTask_InvalidName()
+        {
+            var input = new TaskEntity("Name", 1, TODOAppBE.Common.Status.NotStarted);
+            _sut.Insert(input);
+
+            var res = _sut.Delete("Name2");
 
             Assert.Equivalent(null, res);
         }
